8e507e5233a42477cdd3dbe29b36f0517c829f54
bachaPro <zhouyaofei@gmail.com>
2013-02-07 00:29:22 -0500

    add edit cell

--- wodegrid/app/view/bookgrid.js
+++ wodegrid/app/view/bookgrid.js
@@ -1,33 +1,27 @@
-// var store=Ext.create('Ext.data.Store', {
-//     storeId:'simpsonsStore',
-//     fields:['name', 'email', 'phone'],
-//     data:{'items':[
-//         { 'name': 'Lisa',  "email":"lisa@simpsons.com",  "phone":"555-111-1224"  },
-//         { 'name': 'Bart',  "email":"bart@simpsons.com",  "phone":"555-222-1234" },
-//         { 'name': 'Homer', "email":"home@simpsons.com",  "phone":"555-222-1244"  },
-//         { 'name': 'Marge', "email":"marge@simpsons.com", "phone":"555-222-1254"  }
-//     ]},
-//     proxy: {
-//         type: 'memory',
-//         reader: {
-//             type: 'json',
-//             root: 'items'
-//         }
-//     }
-// });
+
 Ext.require('Ext.grid.RowNumberer');
-Ext.require('Ext.selection.CheckboxModel');
+
 
 Ext.define('wodegrid.view.bookgrid', {
 	extend:'Ext.grid.Panel',
 	alias:'widget.bookgrid',
     title: 'Simpsons',
     store: 'book',
+    require:'Ext.selection.CheckboxModel', 
+    selType: 'cellmodel',
     selModel: Ext.create('Ext.selection.CheckboxModel'), //1
+    plugins: [
+       Ext.create('Ext.grid.plugin.CellEditing', {
+          clicksToEdit: 1
+       })
+   ],
     initComponent:function(){
          this.store='book';
          this.columns=[ Ext.create('Ext.grid.RowNumberer'),
-        { text: 'Name',  dataIndex: 'name' },
+        { text: 'Name',  dataIndex: 'name',editor: {
+         xtype:'textfield',
+         allowBlank:false
+      }},
         { text: 'Email', dataIndex: 'email', flex: 1 },
         { text: 'Phone', dataIndex: 'phone' }
         ];
